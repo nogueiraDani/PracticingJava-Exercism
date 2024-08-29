9ec11662a4c376cd3afc98bc87f6f628ad47f26a
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2024-08-29 14:28:49 -0300

    exercise resolved

---
+++ tim-from-marketing/src/main/java/Badge.java
@@ -0,0 +1,6 @@
+class Badge {
+    public String print(Integer id, String name, String department) {
+        return ((id == null) ? "" : "[" + id + "] - ") + name + " - " +
+                ((department == null) ? "OWNER" : department.toUpperCase());
+    }
+}

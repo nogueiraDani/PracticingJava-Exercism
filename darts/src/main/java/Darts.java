433bfc2bd9ba28d8dfea5a12887bcb613ab708cd
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2024-09-04 20:47:21 -0300

    exercise resolved

---
+++ darts/src/main/java/Darts.java
@@ -0,0 +1,11 @@
+class Darts {
+    int score(double xOfDart, double yOfDart) {
+        double distance = Math.sqrt(xOfDart * xOfDart + yOfDart * yOfDart);
+
+        if (distance > 10) return 0;
+        if (distance > 5) return 1;
+        if (distance > 1) return 5;
+        return 10;
+
+    }
+}
